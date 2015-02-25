cd8eb02f8e5979ed4d26a5b0a4b44f9dd1d19f6e
jsdokos <jsdokos@gmail.com>
2015-02-24 21:31:13 -0600

    Work in progress

--- conditionals2/proj4.java
+++ conditionals2/proj4.java
@@ -7,11 +7,12 @@ public class proj4
 	public static void main(String[] args) 
 	{
 		boolean[][] used = new boolean[4][13];
-		int[][] hand = new int[5][5];
 		Random r = new Random();
 		int suit;
 		int cardval;
-		int j = 0;
+		
+		int[] hand_suit = new int[5];
+		int[] hand_cardval = new int[5];
 		
 		for (int i = 0; i < 5; i++)
 		{
@@ -22,18 +23,64 @@ public class proj4
 				suit =  r.nextInt(4);
 				cardval =  r.nextInt(13);
 				
-				//System.out.println(test++);
+				// puts card in 'hand'
+				hand_suit[i] = suit;
+				hand_cardval[i] = cardval;
 			}
 			while (used[suit][cardval] == true);
 			
 			// sets the one that have been used to true so it won't be used anymore
 			used[suit][cardval] = true;
 			
-
-			//need to find the best way to hold the card suit and value
-			
 			System.out.println(suit + " " + cardval);
 			
+		}
+		//TODO output the cards
+		
+		//bubble sorts the cards and suits
+		
+		boolean swap = true;
+		int temp;
+		
+		//puts the suits in order
+		do
+		{
+			swap = false;
+			for(int i = 0; i < hand_suit.length - 1; i++)
+			{
+				if (hand_suit[i] > hand_suit[i + 1])
+				{
+					temp = hand_suit[i];
+					hand_suit[i] = hand_suit[i + 1];
+					hand_suit[i + 1] = temp;
+					swap = true;
+				}
+			}
+		}
+		while(swap);
+		
+		//puts the cards in order
+		do
+		{
+			swap = false;
+			for(int i = 0; i < hand_cardval.length - 1; i++)
+			{
+				if (hand_cardval[i] > hand_cardval[i + 1])
+				{
+					temp = hand_cardval[i];
+					hand_cardval[i] = hand_cardval[i + 1];
+					hand_cardval[i + 1] = temp;
+					swap = true;
+				}
+			}
+		}
+		while(swap);
+		
+		System.out.println();
+		
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.println(hand_suit[i]+ " " + hand_cardval[i]);
 		}
 		
 	}
